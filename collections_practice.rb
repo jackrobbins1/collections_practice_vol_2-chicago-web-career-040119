f5cc6d39aa5f71461bbb5d530d6527f98008bd27
Jack Robbins <jackrobbins1@gmail.com>
2019-03-24 01:04:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,6 +4,11 @@ def begins_with_r(array)
   yea_o_nea = true
   array.each { |el|
     zplit = el.split("")
+    if zplit[0] == "r"
+      return false
+    else
+      next
+    end
     binding.pry
   }
 end

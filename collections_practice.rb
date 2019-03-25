4223598760fe6dd42595bd6757b06d3794455bb4
Jack Robbins <jackrobbins1@gmail.com>
2019-03-25 16:23:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -118,6 +118,8 @@ dataz = [
 def merge_data(hsh_one, hsh_two)
   #binding.pry
   merge1 = hsh_one[0].merge(hsh_two[0]["blake"])
+  addback = merge1.delete(:motto)
+  merge1[:motto] = addback
   binding.pry
   puts merge1
 end

a5872e1745fa58d6ee15dc8e6f5f1d5c9948688b
Jack Robbins <jackrobbins1@gmail.com>
2019-03-25 15:50:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -104,12 +104,14 @@ dataz = [
        :awesomeness => 10,
             :height => "74",
          :last_name => "johnson"
+     }
   },
   {
     "ashley" => {
         :awesomeness => 9,
              :height => 60,
           :last_name => "dubs"
+    }
   }
   ]
 
